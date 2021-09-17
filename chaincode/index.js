920e22711e60d090296588f1250ef6bf0d730e06
Sai Reddy <40784517+saiobulreddy@users.noreply.github.com>
2021-09-17 10:45:26 +0530

    updated the chain code reference path

--- chaincode/index.js
+++ chaincode/index.js
@@ -1,6 +1,4 @@
 'use strict';
-const SupplyChain = require('./SupplyChain');
-const History = require("./History");
-const Initiation = require("./Initiation");
-module.exports.insuranceContract = { SupplyChain, History, Initiation };
-module.exports.contracts = [SupplyChain, History, Initiation];
+const ChainCode = require('./chaincode');
+module.exports.insuranceContract = { ChainCode };
+module.exports.contracts = [ChainCode];
