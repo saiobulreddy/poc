27933dff1aba95a86e5e08563494e9f6950f4426
Sai Reddy <40784517+saiobulreddy@users.noreply.github.com>
2021-10-05 02:35:15 +0530

    updated with latest code

--- fabric_network/scripts/installChaincode.sh
+++ fabric_network/scripts/installChaincode.sh
@@ -7,7 +7,7 @@ echo "\___ \    | |     / _ \   | |_) |   | |  "
 echo " ___) |   | |    / ___ \  |  _ <    | |  "
 echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
 echo
-echo "Deploying Chaincode REGNET On Certification Network"
+echo "Deploying Chaincode pharmanet On Certification Network"
 echo
 CHANNEL_NAME="$1"
 DELAY="$2"
@@ -36,23 +36,23 @@ echo "Channel name : "$CHANNEL_NAME
 . scripts/utils.sh
 
 ## Install new version of chaincode on peer0 of all 3 orgs making them endorsers
-echo "Installing chaincode on peer0.manufacturer.property-pharma-network.com ..."
+echo "Installing chaincode on peer0.manufacturer.pharma-network.com ..."
 installChaincode 0 'manufacturer' $VERSION
-echo "Installing chaincode on peer0.distributor.property-pharma-network.com ..."
-installChaincode 0 'distributor' $VERSION
-echo "Installing chaincode on peer0.retailer.property-pharma-network.com ..."
-installChaincode 0 'retailer' $VERSION
-echo "Installing chaincode on peer0.consumer.property-pharma-network.com ..."
-installChaincode 0 'consumer' $VERSION
-echo "Installing chaincode on peer0.transporter.property-pharma-network.com ..."
-installChaincode 0 'transporter' $VERSION
-
+# echo "Installing chaincode on peer0.distributor.pharma-network.com ..."
+# installChaincode 0 'distributor' $VERSION
+# echo "Installing chaincode on peer0.retailer.pharma-network.com ..."
+# installChaincode 0 'retailer' $VERSION
+# echo "Installing chaincode on peer0.consumer.pharma-network.com ..."
+# installChaincode 0 'consumer' $VERSION
+# echo "Installing chaincode on peer0.transporter.pharma-network.com ..."
+# installChaincode 0 'transporter' $VERSION
+echo
 # Instantiate chaincode on the channel using peer0.manufacturer
-echo "Instantiating chaincode on channel using peer0.manufacturer.property-pharma-network.com ..."
+echo "Instantiating chaincode on channel using peer0.manufacturer.pharma-network.com ..."
 instantiateChaincode 0 'manufacturer' $VERSION
 
 echo
-echo "========= All GOOD, Chaincode REGNET Is Now Installed & Instantiated On Registration Network =========== "
+echo "========= All GOOD, Chaincode pharmanet Is Now Installed & Instantiated On Registration Network =========== "
 echo
 
 echo
